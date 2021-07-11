f74a5df5578c4b4662c542265078c82a3e8b8271
Ojas Kaul <kaulojas123@gmail.com>
2021-07-11 23:58:21 +0530

    added a button to the homepage image display

--- src/components/item-card/item-card.component.jsx
+++ src/components/item-card/item-card.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './item-card.styles.scss';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 const ItemCard=({item})=>(
     <div className='item-card'>
 
         <div className='image'
         style={{backgroundImage:`url(${item.imageUrl})`}}>
+        <CustomButton>Add to Cart</CustomButton>
         </div>
 
         <div className='footer'>
